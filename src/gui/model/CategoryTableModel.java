bf4efbfa11b1d12ffbfbffe75b19bfa6b52fc612
yzd-403 <404013419@qq.com>
2018-05-16 20:14:16 +0800

    CategoryTableModel

---
+++ src/gui/model/CategoryTableModel.java
@@ -0,0 +1,57 @@
+package gui.model;
+
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryTableModel implements TableModel {
+
+    String[] columnNames = new String[]{"分类名称", "消费次数"};
+    List<String> cs = new ArrayList<>();
+
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        return null;
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return null;
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+
+    }
+
+    @Override
+    public void addTableModelListener(TableModelListener l) {
+
+    }
+
+    @Override
+    public void removeTableModelListener(TableModelListener l) {
+
+    }
+}
